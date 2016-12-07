464cc5555bbd03ff79c116b2e9af8ddc183b9cde
Marcos Siqueira Junior <junior.crvg@gmail.com>
2016-12-07 14:36:30 -0300

    receives store but cant access children of store

--- src/pages/survivor-list.js
+++ src/pages/survivor-list.js
@@ -4,31 +4,27 @@ import Survivor from '../components/survivor';
 import { connect } from 'react-redux';
 import { fetchSurvivors } from '../actions/survivor-actions';
 
-@connect((store) => {
-	return { survivors: [] };
-})
-export default class SurvivorListPage extends React.Component {
+class SurvivorListPage extends React.Component {
 
 	constructor(props) {
-		super(props);
+		super(props)
+		this._getSurvivor = this._getSurvivor.bind(this)
 	}
 
 	render() {
-		var survivors = this._getSurvivors();
-
- 		return (
+		return (
 			<div className="col-md-12 survivor-list">
-				{survivors}
+				
 			</div>
 		);
 	}
 
-	_getSurvivors() {
-		return this.props.survivors.map((survivor) => {
-			return <Survivor
-						 		{...survivor}
-						 		key={this._getKey(survivor.location)} />
-		});
+	_getSurvivor(survivor) {
+		return ( <Survivor
+				 		{...survivor}
+				 		key={this._getKey(survivor.location)} />
+		);
+		
 	}
 
 
@@ -38,6 +34,15 @@ export default class SurvivorListPage extends React.Component {
 
   componentWillMount(){
     this.props.dispatch(fetchSurvivors());
-    this.props.dispatch(fetchSurvivors());
+  	this.setState({
+  		store: this.props.store
+  	});
   }
 }
+
+const mapStateToProps = store => {
+	return { store: store }
+}
+
+export default connect(mapStateToProps)(SurvivorListPage)
+
