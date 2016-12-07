82ec238e8b98ea0ff0a197e4fe9553c953ab4fe7
Marcos Siqueira Junior <junior.crvg@gmail.com>
2016-12-07 14:46:45 -0300

    add actions for survivors

--- src/actions/survivor-actions.js
+++ src/actions/survivor-actions.js
@@ -12,3 +12,56 @@ export function fetchSurvivors(){
 	}
 }
 
+export function fetchSurvivor(id){
+	return function(dispatch) {
+		axios.get('../../api/people/'+id+'.json')
+			.then((res) => {
+				dispatch({ 
+					type: 'FETCH_SURVIVOR',
+					payload: res.data
+				});
+			});
+	}
+}
+
+export function reportSurvivor(my_id, infected_id){
+	return function(dispatch) {
+		axios.post('../../api/people/'+id+'/report_infection.json', { infected: infected_id, id: my_id })
+			.then((res) => {
+				dispatch({ 
+					type: 'REPORT_INFECTED_SURVIVOR',
+					payload: res.data
+				});
+			});
+	}
+}
+
+export function reportSurvivor(my_id, infected_id){
+	return function(dispatch) {
+		axios.post('../../api/people/'+id+'/report_infection.json', { infected: infected_id, id: my_id })
+			.then((res) => {
+				dispatch({ 
+					type: 'REPORT_INFECTED_SURVIVOR',
+					payload: res.data
+				});
+			});
+	}
+}
+
+export function updateSurvivor(survivor){
+	return function(dispatch) {
+		axios.patch('../../api/people/'+survivor.id+'.json', 
+			{ name: survivor.name,
+			  lonlat: survivor.lonlat,
+			  age: survivor.age,
+			  gender: survivor.gender })
+			.then((res) => {
+				dispatch({ 
+					type: 'UPDATE_SURVIVOR',
+					payload: res.data
+				});
+			});
+	}
+}
+
+
