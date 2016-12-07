6cae22b877cd7ac4339e9cb915911050f8801dc2
Marcos Siqueira Junior <junior.crvg@gmail.com>
2016-12-07 14:55:24 -0300

    fix typo on survivor-actions

--- src/actions/survivor-actions.js
+++ src/actions/survivor-actions.js
@@ -36,18 +36,6 @@ export function reportSurvivor(my_id, infected_id){
 	}
 }
 
-export function reportSurvivor(my_id, infected_id){
-	return function(dispatch) {
-		axios.post('../../api/people/'+id+'/report_infection.json', { infected: infected_id, id: my_id })
-			.then((res) => {
-				dispatch({ 
-					type: 'REPORT_INFECTED_SURVIVOR',
-					payload: res.data
-				});
-			});
-	}
-}
-
 export function updateSurvivor(survivor){
 	return function(dispatch) {
 		axios.patch('../../api/people/'+survivor.id+'.json', 
