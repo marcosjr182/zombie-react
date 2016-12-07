4bb8e97fdc40039c9be0285587a940031a3cecbc
Marcos Siqueira Junior <junior.crvg@gmail.com>
2016-12-06 21:14:20 -0300

    add babel plugin to use spread operator

---
+++ src/reducers/survivors-reducer.js
@@ -0,0 +1,15 @@
+export default function reducer (
+	state = {
+		survivors: [],
+		mySurvivor: {},
+		status: true
+	},	
+	action) {
+
+		switch (action.type) {
+			case "FETCH_SURVIVORS": {
+				return { ...state, status: false }
+			}
+		}
+
+}
--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import {applyMiddleware, createStore} from 'redux';
 import logger from 'redux-logger';
 
-//import reducer from './reducers';
+import reducer from './reducers';
 
 const middleware = applyMiddleware(logger());
 
-export default createStore(reducers, middleware);
+export default createStore(reducer, middleware);
